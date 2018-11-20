b0fe0ae9f835b04bf3e716c7867ccfae2f166475
YONA <alenastr12@gmail.com>
2018-11-20 05:33:06 +0200

    #001 main page html(view mode) #002 main page css(view mode)

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*Scroll To Top Button*/
+var goUpButton = document.getElementById('goUpButton')
+window.onscroll = function () {
+  'use strict';
+  if (window.pageYOffset >= 100) {
+    goUpButton.style.display = 'block';
+  } else {
+    goUpButton.style.display = 'none';
+  }
+}
+goUpButton.onclick = function () {
+  'use strict';
+  window.scrollTo(0, 0);
+}
+
+
+
+document.getElementById("dropdownForPresentationName").addEventListener("click", openPresentationPlan);
+function openPresentationPlan() {
+  document.getElementById("presentationPlanForPresentationName").classList.toggle("active");
+}
+
+document.getElementById("dropdownForAgenda").addEventListener("click", openAgenda);
+function openAgenda() {
+  document.getElementById("agendaList").classList.toggle("active");
+}
+
+
+
+
+
+
+
+
